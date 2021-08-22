5b641d577ee708dba3faedf4ce56f450d5744efd
algol007 <adhyformerz@gmail.com>
2021-08-22 20:34:45 +0800

    fix page responsive

--- pages/index.tsx
+++ pages/index.tsx
@@ -56,7 +56,7 @@ const Home: NextPage = () => {
                     />
                   </a>
                 </Link>
-                <div className="hidden md:block">
+                <div className="hidden lg:block">
                   <div className="ml-10 flex items-baseline space-x-4">
                     <Link href="/#who-we-are">
                       <a className="hover:text-primary dark:hover:text-white px-3 py-2 rounded-md font-medium">
@@ -84,7 +84,7 @@ const Home: NextPage = () => {
               <div className="block">
                 <div className="ml-4 flex items-center md:ml-6"></div>
               </div>
-              <div className="-mr-2 flex md:hidden">
+              <div className="-mr-2 flex lg:hidden">
                 <button
                   className="text-gray-800 dark:text-white hover:text-gray-500 inline-flex items-center justify-center p-2 rounded-md focus:outline-none"
                   onClick={showMenu}
@@ -132,8 +132,8 @@ const Home: NextPage = () => {
 
       {/* Hero */}
       <div className="bg-primary py-16">
-        <div className="container mx-auto dark:bg-gray-800 overflow-hidden relative px-4 md:px-0">
-          <div className="text-start md:w-1/2 py-16 pr-4 sm:pr-6 lg:py-16 lg:pr-8 z-20">
+        <div className="container mx-auto dark:bg-gray-800 overflow-hidden relative px-4 lg:px-0">
+          <div className="text-start lg:w-1/2 py-16 pr-4 sm:pr-6 lg:py-16 lg:pr-8 z-20">
             <h2 className="text-3xl font-extrabold text-black dark:text-white sm:text-4xl">
               <span className="block text-white">Talenta Teknologi Lokal</span>
               <span className="block text-white">
@@ -164,7 +164,7 @@ const Home: NextPage = () => {
       {/* Intro */}
       <section id="who-we-are" className="container mx-auto py-16 px-4 md:px-0">
         <div className="max-w-7xl mx-auto bg-white dark:bg-gray-800">
-          <div className="flex flex-wrap md:-mx-8">
+          <div className="flex flex-wrap lg:-mx-8">
             <div className="w-full lg:w-1/2 md:px-8">
               <div className="mb-12 lg:mb-0 pb-12 lg:pb-0 border-b lg:border-b-0">
                 <h2 className="mb-4 text-3xl lg:text-4xl font-bold font-heading dark:text-white">
@@ -266,7 +266,7 @@ const Home: NextPage = () => {
 
       {/* Testimonial */}
       <section id="testimonial" className="container mx-auto py-16">
-        <div className="w-full flex flex-col md:flex-row mb-8 md:mb-0 justify-center items-center">
+        <div className="w-full flex flex-col lg:flex-row mb-8 md:mb-0 justify-center items-center">
           <div className="bg-white dark:bg-gray-800 w-72 h-72 border shadow-lg m-4 rounded-xl p-6">
             <p className="text-gray-600 dark:text-white">
               <span className="font-bold text-primary text-lg">“</span>
