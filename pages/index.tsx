a1c1d5fb1661c097440cbddfee64bcece56356d2
algol007 <adhyformerz@gmail.com>
2021-08-22 20:39:22 +0800

    fix responsive navigation

--- pages/index.tsx
+++ pages/index.tsx
@@ -103,7 +103,7 @@ const Home: NextPage = () => {
               </div>
             </div>
           </div>
-          <div id="toggle-nav" className="md:hidden">
+          <div id="toggle-nav" className="hidden">
             <div className="px-2 pt-2 pb-3 space-y-1 sm:px-3">
               <Link href="/#who-we-are">
                 <a className="text-gray-300 hover:text-gray-800 dark:hover:text-white block px-3 py-2 rounded-md text-base font-medium">
